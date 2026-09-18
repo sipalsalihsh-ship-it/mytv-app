f7b621ac0b54ab99f8dfba1f294d5a89db313b04
sipalsalih <sipal.salih.sh@gmail.com>
2026-09-18 21:44:58 +0300

    Update channel list and HLS player logic

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = 'mytv-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// دامەزراندنی سرویس وۆرکەر و هەڵگرتنی فایلی سەرەکی
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // فێچ کردن بۆ کارکردنی ئۆفلاین
+// وەڵامدانەوەی داواکارییەکان
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
